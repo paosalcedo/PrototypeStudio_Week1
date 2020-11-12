e089acf3100397e71efaf9214c9880f9a68165d6
unknown <paosalcedo@gmail.com>
2020-11-12 00:05:41 -0500

    added some comments to scripts and changed the game name

--- Paolo_Salcedo_Week1/Assets/Scripts/Week03/GameManager_WK3.cs
+++ Paolo_Salcedo_Week1/Assets/Scripts/Week03/GameManager_WK3.cs
@@ -7,6 +7,8 @@ public class GameManager_WK3 : MonoBehaviour {
 	public static GameManager_WK3 instance;
 
 	// Use this for initialization
+
+	//used the Singleton pattern
 	void Start () {
 		if(instance == null){
 			instance = this;
@@ -18,12 +20,12 @@ public class GameManager_WK3 : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if(Input.GetKeyDown(KeyCode.Space)){
+		if(Input.GetKeyDown(KeyCode.Space)){ //Restarts current game
 			SceneManager.LoadScene("Week03_v3");
 			Time.timeScale = 1;
 		}
 
-		if(Input.GetKeyDown(KeyCode.Escape)){
+		if(Input.GetKeyDown(KeyCode.Escape)){ // Exits to main menu
 			SceneManager.LoadScene("Week03_Intro");
 		} 
 	}
